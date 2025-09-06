1ff8e9a7b52fbd78b7302c867c83ed6b09ca6a49
rini <rini@rinici.de>
2025-09-06 20:52:17 -0300

    bot

---
+++ src/commands/eval.ts
@@ -0,0 +1,44 @@
+import { inspect, type InspectOptions } from "util";
+import { defineCommand } from "../commands";
+
+defineCommand({
+  name: "eval",
+  aliases: ["e"],
+  ownerOnly: true,
+  async execute(ctx, code) {
+    let output = "";
+
+    let script = code.replace(/^```(\w+\n)?|```$/g, "");
+    if (script.includes("await")) script = `(async () => { ${script} })()`;
+
+    try {
+      const console = {
+        log(...args: any[]) {
+          let first = true;
+          for (const arg of args) {
+            if (!first) output += " ";
+
+            output += typeof arg == "string" ? arg : inspect(arg, inspectOpts);
+
+            first = false;
+          }
+        },
+      };
+      const { author, channel, content, guild, member } = ctx.message;
+
+      var result = await eval(script);
+    } catch (err) {
+      var result = err as any;
+    }
+
+    output += inspect(result, inspectOpts);
+
+    ctx.reply(
+      output.length < 1000
+        ? "```ansi\n" + output.replace("`", "`\u200b") + "\n```"
+        : { files: [{ name: "output.ansi", contents: Buffer.from(output, "utf8") }] },
+    );
+  },
+});
+
+const inspectOpts: InspectOptions = { colors: true, showProxy: true };
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import "./commands/eval.ts";
+
+import { Client } from "oceanic.js";
+import { registerHandlers } from "./commands.ts";
+
+export const client = new Client({
+  auth: `Bot ${process.env.TOKEN}`,
+  gateway: { intents: ["ALL"] },
+});
+
+client.once("ready", () => console.log(`logged in as @${client.user.tag}`));
+client.on("error", err => console.error(err));
+
+registerHandlers(client);
+
+client.connect();
